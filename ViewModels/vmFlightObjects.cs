f28589a430c5fe0e1080b7399e94536e9254208e
flightontrack <flightontrack@gmail.com>
2017-06-15 10:17:31 -0400

    h

--- ViewModels/vmFlightObjects.cs
+++ ViewModels/vmFlightObjects.cs
@@ -6,8 +6,9 @@ using MVC_Acft_Track.Models;
 
 namespace MVC_Acft_Track.ViewModels
 {
-    public class vmFlight
+    public class vmFlightObjects
     {
+        public int Flightid;
         public Flight Flight;
         public List<GpsLocation> GpsLocations;
         public int RouteId;
@@ -24,7 +25,8 @@ namespace MVC_Acft_Track.ViewModels
         public int? FlightAcftId;
         public string AcftNum = String.Empty;
 
-        public vmFlight(int id) {
+        public vmFlightObjects(int id) {
+            Flightid = id;
             var entities = new Entities();
             Flight = entities.Flights.Find(id);
             FlightName = Flight.FlightName;
@@ -35,15 +37,36 @@ namespace MVC_Acft_Track.ViewModels
             FlightDate = Flight.FlightDate;
             IsShared = Flight.IsShared;
             GpsLocations = entities.GpsLocations.Where(row => row.FlightID == id).OrderBy(row => row.onSessionPointNum).ToList();
-            FlightPltId = Flight.PilotID??0;
-            PltId = entities.Pilots.Find(Flight.PilotID ?? 0).PilotID;
-            if (PltId != null)
+            var pilot = new vmPilot(Flight.PilotID ?? 0);
+            if (pilot.PilotId != null)
             {
-                FlightPilotCode = entities.Pilots.Find(PltId).PilotCode;
-                FlightPilotName = entities.Pilots.Find(PltId).PilotName;
+                PltId = pilot.PilotId;
+                FlightPilotCode = pilot.PilotCode;
+                FlightPilotName = pilot.PilotName;
             }
             FlightAcftId = Flight.AcftID;
             AcftNum = entities.DimAircraftRemotes.Find(FlightAcftId).AcftNum;
+        }
+    }
+
+    public class vmPilot
+    {
+        public int? PilotId;
+        public Pilot thePilot;
+        public string PilotCode;
+        public string PilotName;
+
+        public vmPilot(int id)
+        {
+            var entities = new Entities();
+            var p = entities.Pilots.Find(id);
+            if (p != null)
+            {
+                thePilot = p;
+                PilotId = p.PilotID;
+                PilotCode = thePilot.PilotCode;
+                PilotName = thePilot.PilotName;
+            }
         }
     }
 }
